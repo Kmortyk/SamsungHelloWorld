6847fe3b3fe5e6fdccb7c70bb19e30681a55b6c9
kmortyk <casha.dryaba@mail.ru>
2020-09-19 17:35:57 +0400

    number

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Введите текст:");
-        System.out.println(sc.nextLine());
+
+        System.out.print("Введите число:");
+        int num = sc.nextInt();
+
+        System.out.println(num + 1);
     }
 }
